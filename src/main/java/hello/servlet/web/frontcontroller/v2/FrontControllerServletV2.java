9cb93de9ecf95dce5df9fabc8fef87233a265246
Ho Seok <139951440+momoandsana@users.noreply.github.com>
2024-09-25 08:40:24 +0900

    Update FrontControllerServletV2.java

--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -39,7 +39,7 @@ public class FrontControllerServletV2 extends HttpServlet {
 
         String requestURI = request.getRequestURI(); // url 를 받아옴 http://localhost:8080/front-controller/v1/hello
 
-        ControllerV2 controller = controllerMap.get(requestURI); // url 로 컨트롤러를 찾아. 인터페이스로 꺼내기 때문에 일관성 있게 꺼낼 수 있다
+        ControllerV2 controller = controllerMap.get(requestURI); // 해당 url 에 맞는 컨트롤러를 찾아. 인터페이스로 꺼내기 때문에 일관성 있게 꺼낼 수 있다
         if(controller==null)
         {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
