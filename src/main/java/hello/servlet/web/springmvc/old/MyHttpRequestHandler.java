ceade4b2b13b1fbd9fa70ad514d5640433546c8d
Ho Seok <swiftie52@naver.com>
2024-09-29 17:17:33 +0900

    application.properties 에 prefix suffix(view resolver) 까지 추가

---
+++ src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import java.io.IOException;
+
+// 빔 이름으로 핸들러 찾음
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+}
---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,20 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/*
+ 어노테이션 없어도 컨트롤러임
+ 스프링 빈의 이름을 url pattern 에 맞춤
+ */
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
